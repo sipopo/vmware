ce96fb644c442af4492237b7d654c4379e06d9c4
Sergey Osipov <sevosipov@beeline.ru>
2016-03-09 13:10:37 +0300

    firewall vmware

--- FirewallSetting.sh
+++ FirewallSetting.sh
@@ -11,30 +11,35 @@ export VI_SERVER=vcent.corbina.net
 
 #env | grep -i vi_
 
-for server in `cat $1` 
+for server in `cat $1 | grep -v "^#" ` 
 do
 
    echo $server
 
+   esxcli -h $server network firewall set -d true
+
+
    #echo vm2.kursk.corbina.net
    #esxcli -h $server network firewall ruleset set -a false -r webAccess
 
-   esxcli -h $server network firewall ruleset set -a true -r ntpCLient
+  esxcli -h $server network firewall ruleset set -a true -r ntpClient
 #   esxcli -h $server network firewall ruleset allowedip add -i "85.21.78.0/24" -r ntpClient
-#   esxcli -h $server network firewall ruleset allowedip remove -i "127.0.0.1" -r ntpClient
+  esxcli -h $server network firewall ruleset allowedip remove -i "127.0.0.1" -r ntpClient
 
    #for service in webAccess vSphereClient snmp CIMHttpServer CIMHttpsServer  
-#   for service in webAccess snmp CIMHttpServer CIMHttpsServer
-#   do
-#	echo $service	
-#	esxcli -h $server network firewall ruleset set -a false -r $service
-#   	for ip in 89.179.138.0/24 85.21.106.0/24 83.102.180.0/24
-#   	do
+   for service in vMotion webAccess faultTolerance CIMSLP CIMHttpsServer CIMHttpServer vSphereClient
+   do
+	echo $service	
+	esxcli -h $server network firewall ruleset set -a false -r $service
+   	for ip in 85.21.78.20 85.21.106.0/24 89.179.138.0/24 83.102.180.0/24
+   	do
 #		#echo $ip
-#		esxcli -h $server network firewall ruleset allowedip add -i "$ip" -r $service
-#   	done
-#   done 
+		esxcli -h $server network firewall ruleset allowedip add -i "$ip" -r $service
+   	done
+   done 
 
    esxcli -h $server network firewall ruleset allowedip list
+
+   esxcli -h $server network firewall set -d false
 done
 
